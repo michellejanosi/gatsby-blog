9062708d193d4443172a6ad3badb0a00cfe5d42e
michellejanosi <michelle.janosi@gmail.com>
2020-06-24 10:12:43 -0400

    Add google fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,5 +44,16 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `roboto mono`,
+          `muli\:400,400i,700,700i`,
+          `source sans pro\:300,400,400i,700`, // you can also specify font weights and styles
+        ],
+        display: "swap",
+      },
+    },
   ],
-};
+}
